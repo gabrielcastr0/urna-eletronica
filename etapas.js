21755dbc63d0605b1cb210f1469c60545f3b86ac
Gabriel Castro <gabrielcastro.xy@gmail.com>
2020-10-16 21:59:11 -0300

    Criando controle das áreas da tela

---
+++ etapas.js
@@ -0,0 +1,16 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: "Fulano de Tal",
+        partido: "ABC",
+        fotos: [
+          { url: "./assets/38111.jpg", legenda: "Vereador", small: false },
+        ],
+      },
+    ],
+  },
+];
