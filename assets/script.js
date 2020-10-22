1993dbf3a9aec9d2ac42bbc0a1707c9c404fc5ec
Gabriel Castro <gabrielcastro.xy@gmail.com>
2020-10-22 11:52:17 -0300

    Tela FIM e Salvando Voto

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ let numeros = document.querySelector(".d-1-3");
 let etapaAtual = 0;
 let numero = "";
 let votoBranco = false;
+let votos = [];
 
 function comecarEtapa() {
   let etapa = etapas[etapaAtual];
@@ -109,10 +110,16 @@ function confirma() {
 
   if (votoBranco === true) {
     votoConfirmado = true;
-    console.log("Confirmando como BRANCO...");
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto: "branco",
+    });
   } else if (numero.length === etapa.numeros) {
     votoConfirmado = true;
-    console.log("Confirmando como " + numero);
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto: numero,
+    });
   }
 
   if (votoConfirmado) {
@@ -120,7 +127,9 @@ function confirma() {
     if (etapas[etapaAtual] != undefined) {
       comecarEtapa();
     } else {
-      console.log("FIM");
+      document.querySelector(".tela").innerHTML =
+        '<div class="aviso--gigante pisca">FIM</div>';
+      console.log(votos);
     }
   }
 }
